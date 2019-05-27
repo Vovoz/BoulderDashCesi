dc2923e6ffa7511c90436750a7805a1da614886c
Vovoz <baptiste.marcon@gmail.com>
2019-05-27 17:45:15 +0200

    Commit #4

---
+++ src/main/java/dao/Dao.java
@@ -0,0 +1,12 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class Dao {
+
+    private final static String url = "jdbc:mysql://http://207.154.250.162/phpmyadmin/db_structure.php?server=1&db=boulderdash&token=f6095a84188882f141cf0e872d395775";
+
+    private final static String user = "java";
