e035b6bcd10084e7119326250d432001807edd30
Lionel84100 <lioneldelcasse@gmail.com>
2019-05-30 10:52:38 +0200

    Version finale

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -24,7 +24,6 @@ public abstract class Main {
     public static void main(final String[] args) {
 
 
-
         final Model model = new Model();
         final View view = new View(model);
         final Controller controller = new Controller(view, model);
